c1d9fe341711754e63b969faf6cefec0dc6bc91a
nytouu <capitaine7312@gmail.com>
2023-03-21 17:26:07 +0100

    camping done ?

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 
 // Variables
-int stamina = 100;
+int stamina = 20;
 Location *visited[] = {};
 
 
 // Functions
 Location *locationChoice(Location *first, Location *second);
+int goCamping(Location *where);
 
 
 // Declaration
@@ -24,7 +25,11 @@ main()
 
     visited[0] = place1;
 
+    cout << "Stamina : " << stamina << endl;
+
     Location *result = locationChoice(place2, place3);
+
+    cout << "Stamina : " << stamina << endl;
 
     if (result == 0)
         return 1;
@@ -36,22 +41,49 @@ Location
 *locationChoice(Location *first, Location *second)
 {
     string choice;
+    bool canCamping = false;
+    int restValue = 0;
+    Location *selected;
 
     cout << "Choisissez un chemin: \"" << first->getName() <<  "\" ou \"" << second->getName() << "\" :" << endl;
     cin >> choice;
 
     if (choice == first->getName())
-    {
-        stamina -= first->getDifficulty();
-        visited[1] = first;
-        return first;
-    }
+        selected = first;
     else if (choice == second->getName())
+        selected = second;
+    else return 0;
+
+    if (!selected->getCamping() && (stamina - first->getDifficulty() <= 0))
+        canCamping = true;
+
+    if (canCamping)
     {
-        stamina -= second->getDifficulty();
-        visited[1] = second;
-        return second;
+        char camp;
+        cout << "Would you like to camp in " << selected->getName() << " : y/n" << endl;
+        cin >> camp;
+
+        switch (camp)
+        {
+            case 'y':
+                restValue = goCamping(selected);
+                break;
+            case 'n':
+                cout << "ok then" << endl;
+                break;
+            default:
+                return 0;
+        }
     }
-    else
-        return 0;
+
+    stamina -= selected->getDifficulty() - restValue;
+    visited[1] = selected;
+    return selected;
+}
+
+int
+goCamping(Location *where)
+{
+    cout << "You camped in " << where->getName() << ", you feel rested." << endl;
+    return where->getDifficulty() / 2;
 }
