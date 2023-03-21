625fdf77cddb9f985081597131a44d53c6944060
nytouu <capitaine7312@gmail.com>
2023-03-21 15:28:50 +0100

    choix lieu

--- main.cpp
+++ main.cpp
@@ -2,9 +2,38 @@
 
 using namespace std;
 
+string choix_lieu();
+
 int
 main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    string result = choix_lieu();
+
+    if (result == "Error")
+        return 1;
+    else
+        return 0;
+}
+
+string
+choix_lieu()
+{
+    string choix;
+
+    cout << "Choisissez un chemin: \"Gauche\" ou \"Droite\": " << endl;
+    cin >> choix;
+
+    switch (tolower(choix[0]))
+    {
+        case 'g':
+            cout << "Vous avez choisi le passage à gauche !" << endl;
+            return "gauche";
+        case 'd':
+            cout << "Vous avez choisi le passage à droite !" << endl;
+            return "droite";
+        default:
+            cout << "Choix invalide" << endl;
+            return "Erreur";
+    }
+
 }
